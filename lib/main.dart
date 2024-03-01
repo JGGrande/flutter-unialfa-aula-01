79e7b10b79eff54f4f72923e021871f52ee6e867
JGGrande <joaogabriel7303@gmail.com>
2024-02-29 21:48:08 -0300

    Base do projeto

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:aula_01/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+    title: "Aula01",
+    debugShowCheckedModeBanner: false,
+  ));
+}
